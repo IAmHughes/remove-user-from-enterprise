30faf85571452b654085714144d4f5665cc0fb3f
Thomas Hughes <iamhughes@github.com>
2020-05-05 14:54:47 -0500

    Get initial logic for grabbing all orgs under enterprise

--- index.js
+++ index.js
@@ -1,26 +1,26 @@
 require('dotenv').config()
-const github = require('@octokit/rest')({
-  auth: `token ${process.env.GITHUB_TOKEN}`,
-  previews: [
-    'hellcat-preview'
-  ],
-  // Set this to GHES url or will default to dotcom
-  baseUrl: process.env.GITHUB_API_URL
-})
-const { graphql } = require('@octokit/graphql')
-const graphqlWithAuth = graphql.defaults({
+// const { github } = require('@octokit/rest')({
+//   auth: `token ${process.env.GITHUB_TOKEN}`,
+//   previews: [
+//     'hellcat-preview'
+//   ],
+//   // Set this to GHES url or will default to dotcom
+//   baseUrl: process.env.GITHUB_API_URL
+// })
+let { graphql } = require('@octokit/graphql')
+graphql = graphql.defaults({
   baseUrl: process.env.GITHUB_API_URL,
   headers: {
     authorization: `token ${process.env.GITHUB_TOKEN}`
   }
 })
 
-const organizations
+getOrganizations()
 
-async function getOrgsFromEnterprise () {
-  const { organizations } = await graphqlWithAuth(
-    `query ($enterprise: String!) {
-      enterprise(slug: $enterprise) {
+async function getOrganizations () {
+  const query = {
+    query: `query ($enterprise: String!) {
+      enterprise(slug:$enterprise) {
         name
         organizations(first: 100) {
           nodes {
@@ -29,5 +29,18 @@ async function getOrgsFromEnterprise () {
         }
       }
     }`,
-    enterprise: `${process.env.Enterprise}`
-  )}
+    enterprise: process.env.ENTERPRISE
+  }
+  try {
+    const getOrgResult = await graphql(query)
+    console.log(getOrgResult.enterprise.organizations.nodes[0].login)
+    const orgsObj = getOrgResult.enterprise.organizations.nodes
+
+    orgsObj.forEach((org) => {
+      console.log(org.login)
+    })
+  } catch (error) {
+    console.log('Request failed:', error.request)
+    console.log(error.message)
+  }
+}
